d88a8bbdbd0109e81fff416a9ea2ee579bd11fbc
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-02 22:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 //your JS code here. If required.
 function flatten(value) {
+	// Initialize an empty result array or object, depending on the input type
+  let result;
+  
+  if (Array.isArray(value)) {
+    result = [];
+  } else if (typeof value === 'object' && value !== null) {
+    result = {};
+  } else {
+    // If the input is a primitive value, return it as is
+    return value;
+  }
+	for (const key in value) {
+    if (value.hasOwnProperty(key)) {
+      const flattenedValue = flatten(value[key]);
+
+      if (Array.isArray(result)) {
+        // If the result is an array, concatenate the flattened value
+        result = result.concat(flattenedValue);
+      } else {
+        // If the result is an object, merge the flattened key-value pairs
+        for (const innerKey in flattenedValue) {
+          if (flattenedValue.hasOwnProperty(innerKey)) {
+            result[innerKey] = flattenedValue[innerKey];
+          }
+        }
+      }
+    }
+  }
+
+  return result;
  
 }
 
